5f8fce133a6a4fea101236f365039a7608756614
mjsutcliffe99 <mjsutcliffe1999@gmail.com>
2026-06-12 19:58:13 +0200

    add simple pipeline test

---
+++ methods/pyzx_magic_cat/simulator.py
@@ -0,0 +1,25 @@
+import sys
+import pyzx as zx
+from pyzx.simulation import Strategy
+
+def run(circuit_file: str):
+    with open(circuit_file, "r") as f:
+        seed = int(f.read().strip()) #TEMP (for now, just read a seed rather than a circuit)
+        print("seed:",seed) #TEMP
+    q, d = 10, 300
+    g = zx.generate.cliffordT(qubits=q,depth=d,seed=seed)
+    g.apply_state("0"*q)
+    g.apply_effect("0"*q)
+    zx.simplify.full_reduce(g)
+
+    return zx.simulation.simulate(Strategy.MAGIC_CAT, g)
+
+def main():
+    #if len(sys.argv) != 2:
+    #    print("Usage: simulator.py <circuit_file>")
+    #    sys.exit(1)
+    amplitude = run(sys.argv[1])
+    print(amplitude)
+
+if __name__ == "__main__":
+    main()
