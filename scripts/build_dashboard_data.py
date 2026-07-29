5d4f3e645791faf56ebde0c42173aeb5c0445484
mjsutcliffe99 <mjsutcliffe1999@gmail.com>
2026-07-30 01:33:23 +0200

    Remove "unplugged" Pyzx reduced T-count
    Going forward, the random circuits generated by Pyzx will be recorded after running teleport_reduce. Consequently, we no longer need to track the "Pyzx unplugged reduced T-count" and the "Pyzx plugged reduced T-count" but rather the former will be the new "raw" T-count and the latter will now be labelled simply "Pyzx reduced T-count" (i.e. T-count after plugging inputs/outputs and running full_reduce)

--- scripts/build_dashboard_data.py
+++ scripts/build_dashboard_data.py
@@ -408,13 +408,6 @@ def build_circuit_entry(
         "pyzx_reduced_T_count",
     )
 
-    pyzx_plugged_reduced_t_count = first_present(
-        metrics,
-        "pyzx_plugged_reduced_t_count",
-        "pyzx_plugged_reduced_tcount",
-        "pyzx_plugged_reduced_T_count",
-    )
-
     if circuit_class is None:
         raise DashboardBuildError(
             f"{metadata_path}: missing required field "
@@ -433,7 +426,6 @@ def build_circuit_entry(
         "gate_count": gate_count,
         "t_count": t_count,
         "pyzx_reduced_t_count": pyzx_reduced_t_count,
-        "pyzx_plugged_reduced_t_count": pyzx_plugged_reduced_t_count,
     }
 
     missing_fields = [
@@ -481,11 +473,6 @@ def build_circuit_entry(
         "pyzx_reduced_t_count": number_or_none(
             pyzx_reduced_t_count,
             field_name="metrics.pyzx_reduced_t_count",
-            source=metadata_path,
-        ),
-        "pyzx_plugged_reduced_t_count": number_or_none(
-            pyzx_plugged_reduced_t_count,
-            field_name="metrics.pyzx_plugged_reduced_t_count",
             source=metadata_path,
         ),
     }
